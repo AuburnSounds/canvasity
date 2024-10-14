c9a1ff7cf64eae7f8d9d2ac1bd90985f3fced870
Guillaume Piolat <contact@auburnsounds.com>
2024-10-14 17:13:38 +0200

    Okay

--- examples/rectangle/source/main.d
+++ examples/rectangle/source/main.d
@@ -4,12 +4,15 @@ import gamut;
 void main()
 {
     Image image;
-    image.createNoInit(300, 300);
+    image.create(250, 250, PixelType.rgba8);
 
-    Canvasity context = Canvasity(image);
-
-    context.fillStyle("blue");
-    context.fillRect(50, 50, 200, 100);
+    Canvasity canvas = Canvasity(image);
+    canvas.fillStyle = "#fff";
+    canvas.fillRect(0, 0, 250, 250);
+    canvas.fillStyle("red");
+    canvas.fillRect(140, 20, 40, 250);
+    canvas.fillStyle("blue");
+    canvas.fillRect(50, 50, 150, 100);
 
     image.saveToFile("output-rectangle.png");
 }
